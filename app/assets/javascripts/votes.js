a298d800bd6a910fc4c9a825aa9dac7b7c3ba06c
jford8820 <jford8820@gmail.com>
2015-07-08 17:57:08 -0400

    added ajax for downvotes

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -25,30 +25,31 @@ $(document).ready(function() {
       }
     });
   });
-  $(".downvote a").click(function( event ) {
-    event.preventDefault();
-    event.stopPropagation();
+  $(document).ready(function() {
+    $(".downvote").on("click", function( event ) {
+      event.preventDefault();
+      event.stopPropagation();
 
-    var reviewId = event.toElement.className.slice(14);
+      var reviewId = event.toElement.className.slice(14);
 
-    var score = document.getElementById("review-" + reviewId + "-score").innerHTML;
-    score = parseInt(score);
-    score--;
-    document.getElementById("review-" + reviewId + "-score").textContent = score;
-    // also hide downvote button
-    var url = '/reviews/' + reviewId + '/downvotes';
+      var score = document.getElementById("review-" + reviewId + "-score").innerHTML;
+      score = parseInt(score);
+      score++;
+      document.getElementById("review-" + reviewId + "-score").textContent = score;
 
-    $.ajax({
-      type: "POST",
-      url: url,
-      dataType: "json",
-      complete: function(downvote) {
-        var downvoteId = JSON.parse(downvote.responseText)["id"];
-        var reviewClass = ".downvote .review-" + reviewId + " a";
-        $(reviewClass).attr("data-method", "delete");
-        $(reviewClass).attr("href", "/reviews/" + reviewId + "/downvotes/" + downvoteId);
-        $(reviewClass).text("Delete Upvote");
-      }
+      // also hide downvote button
+      var url = '/reviews/' + reviewId + '/downvotes';
+      $.ajax({
+        type: "POST",
+        url: url,
+        dataType: "json",
+        complete: function(downvote) {
+          var downvoteId = JSON.parse(downvote.responseText)["id"];
+          var reviewClass = ".downvote.review-" + reviewId;
+          $(reviewClass).attr("data-method", "delete");
+          $(reviewClass).attr("href", "/reviews/" + reviewId + "/downvotes/" + downvoteId);
+          $(reviewClass).text("Delete Downvote");
+        }
+      });
     });
-  });
 });
