c818153efb4e5b3936e6ba0803a8f06bf4b95e87
jford8820 <jford8820@gmail.com>
2015-07-09 17:59:56 -0400

    hound

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -17,11 +17,11 @@ $(document).ready(function() {
       document.getElementById("review-" + reviewId + "-score").textContent = score;
 
       // also hide upvote button
-      var post_url = "/reviews/" + reviewId + "/upvotes";
+      var postUrl = "/reviews/" + reviewId + "/upvotes";
 
       $.ajax({
         type: "POST",
-        url: post_url,
+        url: postUrl,
         dataType: "json",
         complete: function() {
           var reviewClass = ".review-" + reviewId + ".upvote";
@@ -29,7 +29,7 @@ $(document).ready(function() {
           $(reviewClass).text("Delete Upvote");
         }
       });
-    }else{
+    } else {
       event.preventDefault();
       event.stopPropagation();
 
@@ -40,11 +40,11 @@ $(document).ready(function() {
       var upvoteId = $(reviewClass).attr("href").match(/\d+/)[0];
 
       // also hide upvote button
-      var del_url = "/reviews/" + reviewId + "/upvotes/" + upvoteId;
+      var delUrl = "/reviews/" + reviewId + "/upvotes/" + upvoteId;
 
       $.ajax({
         type: "DELETE",
-        url: del_url,
+        url: delUrl,
         dataType: "json",
         complete: function() {
           $(reviewClass).attr("data-method", "post");
@@ -71,11 +71,11 @@ $(document).ready(function() {
       document.getElementById("review-" + reviewId + "-score").textContent = score;
 
       // also hide downvote button
-      var post_url = "/reviews/" + reviewId + "/downvotes";
+      var postUrl = "/reviews/" + reviewId + "/downvotes";
 
       $.ajax({
         type: "POST",
-        url: post_url,
+        url: postUrl,
         dataType: "json",
         complete: function() {
           var reviewClass = ".review-" + reviewId + ".downvote";
@@ -83,7 +83,7 @@ $(document).ready(function() {
           $(reviewClass).text("Delete Downvote");
         }
       });
-    }else{
+    } else {
       event.preventDefault();
       event.stopPropagation();
 
@@ -94,11 +94,11 @@ $(document).ready(function() {
       var downvoteId = $(reviewClass).attr("href").match(/\d+/)[0];
 
       // also hide downvote button
-      var del_url = "/reviews/" + reviewId + "/downvotes/" + downvoteId;
+      var delUrl = "/reviews/" + reviewId + "/downvotes/" + downvoteId;
 
       $.ajax({
         type: "DELETE",
-        url: del_url,
+        url: delUrl,
         dataType: "json",
         complete: function() {
           $(reviewClass).attr("data-method", "post");
